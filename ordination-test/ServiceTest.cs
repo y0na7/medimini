d88f7b16b55f9b3ab76dac9348b646a30389c7de
Kristian Dorland <kristian@dorland.dk>
2022-11-29 09:46:08 +0100

    Lidt flere eksempler på tests.

--- ordination-test/ServiceTest.cs
+++ ordination-test/ServiceTest.cs
@@ -4,16 +4,18 @@ using Microsoft.EntityFrameworkCore;
 
 using Service;
 using Data;
+using shared.Model;
 
 [TestClass]
 public class ServiceTest
 {
-    private readonly DataService service;
+    private DataService service;
 
-    public ServiceTest()
+    [TestInitialize]
+    public void SetupBeforeEachTest()
     {
         var optionsBuilder = new DbContextOptionsBuilder<OrdinationContext>();
-        optionsBuilder.UseInMemoryDatabase(databaseName: "test-database"); 
+        optionsBuilder.UseInMemoryDatabase(databaseName: "test-database");
         var context = new OrdinationContext(optionsBuilder.Options);
         service = new DataService(context);
         service.SeedData();
@@ -23,5 +25,19 @@ public class ServiceTest
     public void PatientsExist()
     {
         Assert.IsNotNull(service.GetPatienter());
+    }
+
+    [TestMethod]
+    public void OpretDagligFast()
+    {
+        Patient patient = service.GetPatienter().First();
+        Laegemiddel lm = service.GetLaegemidler().First();
+
+        Assert.AreEqual(1, service.GetDagligFaste().Count());
+
+        service.OpretDagligFast(patient.PatientId, lm.LaegemiddelId,
+            2, 2, 1, 0, DateTime.Now, DateTime.Now.AddDays(3));
+
+        Assert.AreEqual(2, service.GetDagligFaste().Count());
     }
 }
